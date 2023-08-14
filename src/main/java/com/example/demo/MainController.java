64d0281079b6428f958755e024b77f604498af42
AnUser99 <kisamasyanya@gmail.com>
2023-08-14 07:10:11 +0300

    spring_quick 8_1

--- src/main/java/com/example/demo/MainController.java
+++ src/main/java/com/example/demo/MainController.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller // Стереотипная аннотация @Controller говорит, что класс контроллера Spring MVC
@@ -8,8 +9,12 @@ public class MainController {
 
     @RequestMapping("/home") // аннотация привязывает действие
             // к пути /home, который содержится в HTTP-запросе
-    public String home() {
-        return "home.html"; // строка с именем документа,
-            // который приложение будет возвращать в ответ на запрос
+    public String home(Model page) { // в параметре типа Model хранятся
+            // данные, передаваемые контроллером в представление
+        // Добавим данные, которые контроллер должен отправить в представление
+        page.addAttribute("username", "Katy");
+        page.addAttribute("color", "red");
+        return "home.html"; // строка с именем представления,
+            // которое затем будет преобразовано в HTTP-ответ
     }
 }
